7e5bd5ac7a55a705519e7a849fe6e1c5b4080534
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-20 21:52:04 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,9 +11,9 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get("SESSION", "forward bot")
-API_ID = int(environ["API_ID", "25163484"])
-API_HASH = environ["API_HASH", "145bcbc424d1c1ffe04f3e607ea55c9a"]
-BOT_TOKEN = environ["BOT_TOKEN", "7016509767:AAHGyELmvA9D6uCz2yPsI5sL4gr7pooU1lI"]
+API_ID = 25163484
+API_HASH = "145bcbc424d1c1ffe04f3e607ea55c9a"
+BOT_TOKEN = "7016509767:AAHGyELmvA9D6uCz2yPsI5sL4gr7pooU1lI"
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002137528664"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6302921275').split()]
