a871051d42322e5706fd11a7deaf8e8f8a72c09e
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-25 15:08:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 SESSION = environ.get("SESSION", "forward bot")
 API_ID = 25163484
 API_HASH = "145bcbc424d1c1ffe04f3e607ea55c9a"
-BOT_TOKEN = "6556254534:AAFOhanY2d6X3PJ7P4MHDTyxyUVpgvYUDns"
+BOT_TOKEN = "7016509767:AAHGyELmvA9D6uCz2yPsI5sL4gr7pooU1lI"
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002137528664"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6302921275').split()]
-TARGET_DB = int(environ.get("TARGET_DB", "-1002137528664"))
+TARGET_DB = int(environ.get("TARGET_DB", "-1002239734551"))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Joelkb/File-Forward-Bot")
