efdd3818120940882b2a62a3c5c3bea6982dbc91
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 10:11:23 +0530

    Update auto_forward.py

--- plugins/auto_forward.py
+++ plugins/auto_forward.py
@@ -1,7 +1,8 @@
+import asyncio
 from pyrogram import Client, filters
 from vars import ADMINS, FROM_DB, TARGET_DB
 
-media_filter = filters.document | filters.video | filters.audio
+media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(FROM_DB) & media_filter)
 async def auto_forward(bot, message):
