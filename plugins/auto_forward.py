5a4d50597212740031df0ea0b9331efaab5eb12a
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 11:14:17 +0530

    added[] in resub and added asyncio

--- plugins/auto_forward.py
+++ plugins/auto_forward.py
@@ -6,8 +6,9 @@ media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(FROM_DB) & media_filter)
 async def auto_forward(bot, message):
-    file_caption = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(message.caption))
+    file_caption = re.sub(r"@\w+|(_|\-|\.|\+\[|\])", " ", str(message.caption))
     await message.copy(
-        chat_id=int(TARGET_DB),
-        caption=file_caption
-    )
+            chat_id=int(TARGET_DB),
+            caption=file_caption
+        )
+        await asyncio.sleep(1)
