834c32f0fd21822dbeabe9fbfbc59ee7225b2a5f
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 16:47:35 +0530

    try ek aur baar

--- plugins/auto_forward.py
+++ plugins/auto_forward.py
@@ -7,13 +7,15 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 media_filter = filters.document | filters.video
+lock = asyncio.Lock()
 
 @Client.on_message(filters.chat(FROM_DB) & media_filter)
 async def auto_forward(bot, message):
-    file_caption = re.sub(r"(JOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+|\[|\]\ )", " ", str(message.caption))
-    await message.copy(
-            chat_id=int(TARGET_DB),
-            caption=file_caption
-        )
-    logger.info("Forwarded", message.caption, "from", FROM_DB, "to", TARGET_DB)
-    await asyncio.sleep(1)
+    async with lock:
+        file_caption = re.sub(r"(JOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+|\[|\]\ )", " ", str(message.caption))
+        await message.copy(
+                chat_id=int(TARGET_DB),
+                caption=file_caption
+            )
+        logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TARGET_DB}")
+        await asyncio.sleep(1)
