f185f90e416ecf72751959c72e8493db78478925
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 16:25:27 +0530

    try ek baar

--- plugins/auto_forward.py
+++ plugins/auto_forward.py
@@ -10,10 +10,16 @@ media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(FROM_DB) & media_filter)
 async def auto_forward(bot, message):
+    for file_type in ("document", "video", "audio"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
     file_caption = re.sub(r"(JOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+|\[|\]\ )", " ", str(message.caption))
     await message.copy(
             chat_id=int(TARGET_DB),
             caption=file_caption
         )
-    logger.info("Forwarded", message.caption, "from", FROM_DB, "to", TARGET_DB)
+    logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TARGET_DB}")
     await asyncio.sleep(1)
