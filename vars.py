5b3c72d7a99c095beb52aa54bf6b139181059582
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 21:17:08 +0530

    fixed i guess

--- vars.py
+++ vars.py
@@ -17,6 +17,6 @@ BOT_TOKEN = environ.get("BOT_TOKEN")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002137528664"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6302921275').split()]
-FROM_DB = int(environ.get("FROM_DB", "-1002369988753")).split()
+FROM_DB = [int(channel_id) for channel_id in environ.get('FROM_DB', '-1002369988753').split() if re.match(r'^-?\d+$', channel_id)]
 TARGET_DB = int(environ.get("TARGET_DB", "-1002399329080"))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/MandaliWala/File-Forward-Bottermux")
