b104bf45429324ebaad25e479f235e04b1020403
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 10:27:33 +0530

    try resub caption

--- plugins/auto_forward.py
+++ plugins/auto_forward.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, re
 from pyrogram import Client, filters
 from vars import ADMINS, FROM_DB, TARGET_DB
 
@@ -6,6 +6,8 @@ media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(FROM_DB) & media_filter)
 async def auto_forward(bot, message):
+    file_caption = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(message.caption))
     await message.copy(
         chat_id=int(TARGET_DB)
+        caption=file_caption
     )
