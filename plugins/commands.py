68c2185b2436b0ccd7538aaf077bb685e4ab94c3
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-22 12:46:16 +0530

    added m2links resub in forward

--- plugins/commands.py
+++ plugins/commands.py
@@ -123,7 +123,7 @@ async def start_forward(bot, userid, source_chat_id, last_msg_id):
     unsupported = 0
     left = 0
     status = 'Idle'
-    file_caption = re.sub(r"@\w+|(_|\- |\.|\+|\[|\]\ )|(JOIN 💎 : @M2LINKS)", " ", str(message.caption))
+    file_caption = re.sub(r"(JOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+|\[|\]\ )", " ", str(msg.caption))
     async with lock:
         try:
             btn = [[
@@ -170,7 +170,8 @@ async def start_forward(bot, userid, source_chat_id, last_msg_id):
                     continue
                 try:
                     await msg.copy(
-                        chat_id=int(TARGET_DB)
+                        chat_id=int(TARGET_DB),
+                        caption=file_caption
                     )
                     forwarded+=1
                     await asyncio.sleep(1)
@@ -184,7 +185,8 @@ async def start_forward(bot, userid, source_chat_id, last_msg_id):
                     )
                     await asyncio.sleep(e.value)
                     await msg.copy(
-                        chat_id=int(TARGET_DB)
+                        chat_id=int(TARGET_DB),
+                        caption=file_caption
                     )
                     forwarded+=1
                     continue
