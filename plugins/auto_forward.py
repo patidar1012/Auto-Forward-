8e979570ec6a7ec07296fd4520efc0b840ded9d0
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-30 09:18:50 +0530

    sperated auto_forward and manual forward channels

--- plugins/auto_forward.py
+++ plugins/auto_forward.py
@@ -1,7 +1,7 @@
 import asyncio, re
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from vars import FROM_DB, TARGET_DB
+from vars import FROM_DB, TO_DB
 
 import logging
 logger = logging.getLogger(__name__)
@@ -25,11 +25,11 @@ async def auto_forward(bot, message):
     async with lock:
         try:
             await message.copy(
-                    chat_id=int(TARGET_DB),
+                    chat_id=int(TO_DB),
                     caption=file_caption
                 )
             forwarded += 1
-            logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TARGET_DB}\nforwarded {forwarded} files")
+            logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TO_DB}\nforwarded {forwarded} files")
             await asyncio.sleep(1)
             if forwarded % 20 == 0:
                 logger.info("⏸️ 20 files sent! Taking a break of 30 seconds...")
@@ -38,11 +38,11 @@ async def auto_forward(bot, message):
             logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
             await asyncio.sleep(e.value + 2)
             await message.copy(
-                    chat_id=int(TARGET_DB),
+                    chat_id=int(TO_DB),
                     caption=file_caption
                 )
             forwarded += 1
-            logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TARGET_DB}\nforwarded {forwarded} files")
+            logger.info(f"Forwarded {message.caption} from {FROM_DB} to {TO_DB}\nforwarded {forwarded} files")
             await asyncio.sleep(1)
             if forwarded % 20 == 0:
                 logger.info("⏸️ 20 files sent! Taking a break of 30 seconds...")
