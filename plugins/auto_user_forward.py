16a6c1c756e806a74610dcf4d3295fabb3cc37e1
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-06-11 17:45:19 +0530

    Revert "try marte hain"
    This reverts commit a8282c4ce0d01973de9119cd3a39490daa2de7dc.

--- plugins/auto_user_forward.py
+++ plugins/auto_user_forward.py
@@ -1,7 +1,6 @@
 import asyncio, re
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from script import scripts
 from vars import API_ID, API_HASH, USER_SESSION, USER_FROM_DB, USER_TO_DB
 
 import logging
@@ -16,41 +15,40 @@ class CLIENT(Client):
   async def user_session(self):
     user = Client(API_ID, API_HASH, session_string=USER_SESSION)
     await user.connect()
-    await self.send_message(chat_id=LOG_CHANNEL, text=scripts.RESTART_TXT.format(today, time))
 
 media_filter = filters.document | filters.video
 lock = asyncio.Lock()
 forwarded = 0
 
-# @Client.on_message(filters.chat(USER_FROM_DB) & media_filter)
-# async def auto_forward(client, message):
-#     global forwarded
-#     file_caption = re.sub(r"(❤️‍🔥 Join ~ [ @Moonknight_media ])|(\n🔸 Upload By \[@BlackDeath_0\])|(\n❤️‍🔥 Join ~ \[@Moonknight_media\])|(@Ac_Linkzz)|(\n⚡️Join:- \[@BlackDeath_0\]‌‌)|(EonMovies)|(\nJOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+)", " ", str(message.caption))
-#     file_caption = f"""{file_caption}\n➖➖➖➖➖➖➖➖➖➖
-#  <b>Powered By:</b> <a href='https://t.me/MovieTimesTV'><b>Mᴏᴠɪᴇ Tɪᴍᴇs™</b></a>
-# ➖➖➖➖➖➖➖➖➖➖"""
-#     async with lock:
-#         try:
-#             await message.copy(
-#                     chat_id=int(USER_TO_DB),
-#                     caption=file_caption
-#                 )
-#             forwarded += 1
-#             logger.info(f"Forwarded {message.caption} from {USER_FROM_DB} to {USER_TO_DB}\nforwarded {forwarded} files")
-#             await asyncio.sleep(1)
-#             if forwarded % 20 == 0:
-#                 logger.info("⏸️ 20 files sent! Taking a break of 30 seconds...")
-#                 await asyncio.sleep(30)
-#         except FloodWait as e:
-#             logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
-#             await asyncio.sleep(e.value + 2)
-#             await message.copy(
-#                     chat_id=int(USER_TO_DB),
-#                     caption=file_caption
-#                 )
-#             forwarded += 1
-#             logger.info(f"Forwarded {message.caption} from {USER_FROM_DB} to {USER_TO_DB}\nforwarded {forwarded} files")
-#             await asyncio.sleep(1)
-#             if forwarded % 20 == 0:
-#                 logger.info("⏸️ 20 files sent! Taking a break of 30 seconds...")
-#                 await asyncio.sleep(30)
+@Client.on_message(filters.chat(USER_FROM_DB) & media_filter)
+async def auto_forward(client, message):
+    global forwarded
+    file_caption = re.sub(r"(❤️‍🔥 Join ~ [ @Moonknight_media ])|(\n🔸 Upload By \[@BlackDeath_0\])|(\n❤️‍🔥 Join ~ \[@Moonknight_media\])|(@Ac_Linkzz)|(\n⚡️Join:- \[@BlackDeath_0\]‌‌)|(EonMovies)|(\nJOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+)", " ", str(message.caption))
+    file_caption = f"""{file_caption}\n➖➖➖➖➖➖➖➖➖➖
+ <b>Powered By:</b> <a href='https://t.me/MovieTimesTV'><b>Mᴏᴠɪᴇ Tɪᴍᴇs™</b></a>
+➖➖➖➖➖➖➖➖➖➖"""
+    async with lock:
+        try:
+            await message.copy(
+                    chat_id=int(USER_TO_DB),
+                    caption=file_caption
+                )
+            forwarded += 1
+            logger.info(f"Forwarded {message.caption} from {USER_FROM_DB} to {USER_TO_DB}\nforwarded {forwarded} files")
+            await asyncio.sleep(1)
+            if forwarded % 20 == 0:
+                logger.info("⏸️ 20 files sent! Taking a break of 30 seconds...")
+                await asyncio.sleep(30)
+        except FloodWait as e:
+            logger.warning(f"Got FloodWait.\n\nWaiting for {e.value} seconds.")
+            await asyncio.sleep(e.value + 2)
+            await message.copy(
+                    chat_id=int(USER_TO_DB),
+                    caption=file_caption
+                )
+            forwarded += 1
+            logger.info(f"Forwarded {message.caption} from {USER_FROM_DB} to {USER_TO_DB}\nforwarded {forwarded} files")
+            await asyncio.sleep(1)
+            if forwarded % 20 == 0:
+                logger.info("⏸️ 20 files sent! Taking a break of 30 seconds...")
+                await asyncio.sleep(30)
