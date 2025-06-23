65c393d75928fe978705c4b5cd469af4ccf21602
patidar1012 <arpitgavla123@gmail.com>
2025-06-23 12:24:13 +0530

    Update auto_user_forward.py

--- plugins/auto_user_forward.py
+++ plugins/auto_user_forward.py
@@ -19,7 +19,7 @@ async def auto_forward(bot, message):
     global forwarded, count, mcount
     file_caption = re.sub(r"(𝗧𝗛𝗘 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗢𝗥 )|(𝙿𝚘𝚠𝚎𝚛e𝚍 𝙱𝚢 ➥  @Theprofessers)|(❤️‍🔥 Join ~ [ @Moonknight_media ])|(\n🔸 Upload By \[@BlackDeath_0\])|(\n❤️‍🔥 Join ~ \[@Moonknight_media\])|(@Ac_Linkzz)|(\n⚡️Join:- \[@BlackDeath_0\]‌‌)|(EonMovies)|(\nJOIN 💎 : @M2LINKS)|@\w+|(_|\- |\.|\+)", " ", str(message.caption))
     file_caption = f"""{file_caption}\n➖➖➖➖➖➖➖➖➖➖
- <b>Powered By:</b> <a href='https://t.me/MovieTimesTV'><b>Mᴏᴠɪᴇ Tɪᴍᴇs™</b></a>
+ <b>Powered By:</b> <a href='https://t.me/Film_CityOfficial'><b>ꜰɪʟᴍ ᴄɪᴛʏ ᴏꜰꜰɪᴄɪᴀʟ™</b></a>
 ➖➖➖➖➖➖➖➖➖➖"""
     async with lock:
         try:
