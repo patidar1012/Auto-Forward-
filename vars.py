c4c2b47b70f97d7810e159a1de32c8eadc97de80
patidar1012 <arpitgavla123@gmail.com>
2025-06-24 00:36:31 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -18,7 +18,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002074744533"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7870979920').split()]
-TARGET_DB = int(environ.get("TARGET_DB", "-1002884674953"))
+TARGET_DB = int(environ.get("TARGET_DB", "-1002222465571"))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/patidar1012/Auto-Forward-.git")
 #Auto Forward vars
 FROM_DB = [int(channel_id) for channel_id in environ.get('FROM_DB', '-1002414255000 -1002660675955 -1002234633741 -1002884674953').split() if re.match(r'^-?\d+$', channel_id)]
