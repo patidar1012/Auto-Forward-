696c6cb1fb449a20a78571f92b946302b153ee3e
patidar1012 <arpitgavla123@gmail.com>
2025-08-22 22:20:09 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -12,14 +12,15 @@ def is_enabled(value, default):
 
 SESSION = environ.get("SESSION", "N2 forward bot")
 USER_SESSION = environ.get("USER_SESSION", "") #Pyrogram session dalna hai yahan
-API_ID = 27732327
-API_HASH = "7c7c0a2b3b5dc1eb6b84433228dcf4c8"
+API_ID = 24770554
+API_HASH = "6f17857655c644baed6ecca72bbce7f6"
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002074744533"))
 PORT = int(environ.get("PORT", "8080"))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7870979920').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6571116013').split()]
 TARGET_DB = int(environ.get("TARGET_DB", "-1002222465571"))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/patidar1012/Auto-Forward-.git")
 #Auto Forward vars
-FROM_DB = [int(channel_id) for channel_id in environ.get('FROM_DB', '-1002414255000 -1002660675955 -1002234633741 -1002884674953 -1002564944280').split() if re.match(r'^-?\d+$', channel_id)]
+FROM_DB = [int(channel_id) for channel_id in environ.get('FROM_DB', '-1002031007752 -1002988853668').split() if re.match(r'^-?\d+$', channel_id)]
 TO_DB = int(environ.get("TO_DB", "-1002222465571"))
+
